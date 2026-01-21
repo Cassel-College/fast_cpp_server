cbb70a1041cacb2c6d4be7aad29797eef918d3c6
liupeng <liupeng.0@outlook.com>
2026-01-22 01:13:44 +0800

    fix:添加MQTT模块

--- cmake/deps/setup_mosquitto.cmake
+++ cmake/deps/setup_mosquitto.cmake
@@ -4,10 +4,11 @@
 # set(WITH_SHARED_LIBRARIES ON CACHE BOOL "" FORCE) 
 # add_subdirectory(external/mosquitto EXCLUDE_FROM_ALL)
 
-set(WITH_APPS               OFF         CACHE BOOL      "" FORCE)    # 不编译 mosquitto_pub ?等
-set(WITH_STATIC_LIBRARIES   OFF         CACHE BOOL      "" FORCE)
-set(INSTALL_HEADERS         OFF         CACHE BOOL      "" FORCE) # 尝试关闭头文件安装
-set(DOCUMENTATION           OFF         CACHE BOOL      "Disable documentation generation")
+set(WITH_STATIC_LIBRARIES   ON          CACHE BOOL "" FORCE) # 开启静态库编译 (.a)
+set(WITH_SHARED_LIBRARIES   OFF         CACHE BOOL "" FORCE) # 如果你只需要静态库，可以关闭共享库
+set(WITH_APPS               OFF         CACHE BOOL "" FORCE) # 不编译 mosquitto_pub/sub 等工具
+set(WITH_BROKER             ON          CACHE BOOL "" FORCE) # 如果你只需要 Client 库，不编译服务端二进制
+set(DOCUMENTATION           OFF         CACHE BOOL "Disable documentation generation")
 set(DCMAKE_INSTALL_SBINDIR  "bin"       CACHE STRING    "Set sbin directory") # 设置 bin 目录
 
 if (NOT EXISTS "${CMAKE_SOURCE_DIR}/external/mosquitto/CMakeLists.txt")
@@ -22,4 +23,5 @@ else()
         list(APPEND THIRD_INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR}/external/mosquitto/include)
     endif()
 endif()
-add_subdirectory(external/mosquitto)
+# 使用 EXCLUDE_FROM_ALL 可以防止在执行 'make install' 时把整个 mosquitto 装进系统目录
+add_subdirectory(external/mosquitto EXCLUDE_FROM_AL)
