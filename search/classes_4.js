fc0dee752be05187dbb4ecb18f52a1c06116e56c
liupengzhouyi <liupengzhouyi@users.noreply.github.com>
2026-01-27 13:02:04 +0000

    deploy: 00cf970815e45dcbd14a9a5308f8ed937e17b26b

--- search/classes_4.js
+++ search/classes_4.js
@@ -4,7 +4,8 @@ var searchData=
   ['edgecontroller_1',['EdgeController',['../classmy__api_1_1edge_1_1EdgeController.html',1,'my_api::edge']]],
   ['edgedevice_2',['EdgeDevice',['../classedge__manager_1_1EdgeDevice.html',1,'edge_manager']]],
   ['edgenotfoundexception_3',['EdgeNotFoundException',['../classmy__edge_1_1EdgeNotFoundException.html',1,'my_edge']]],
-  ['edgestatus_4',['EdgeStatus',['../structmy__data_1_1EdgeStatus.html',1,'my_data']]],
-  ['edgestatusdto_5',['EdgeStatusDto',['../classmy__api_1_1dto_1_1EdgeStatusDto.html',1,'my_api::dto']]],
-  ['example_6',['Example',['../classExample.html',1,'']]]
+  ['edgescontroller_4',['EdgesController',['../classmy__api_1_1edge_1_1EdgesController.html',1,'my_api::edge']]],
+  ['edgestatus_5',['EdgeStatus',['../structmy__data_1_1EdgeStatus.html',1,'my_data']]],
+  ['edgestatusdto_6',['EdgeStatusDto',['../classmy__api_1_1dto_1_1EdgeStatusDto.html',1,'my_api::dto']]],
+  ['example_7',['Example',['../classExample.html',1,'']]]
 ];
--- search/files_4.js
+++ search/files_4.js
@@ -4,9 +4,11 @@ var searchData=
   ['edgecontroller_2ehpp_1',['EdgeController.hpp',['../EdgeController_8hpp.html',1,'']]],
   ['edgedevice_2ecpp_2',['EdgeDevice.cpp',['../EdgeDevice_8cpp.html',1,'']]],
   ['edgedevice_2eh_3',['EdgeDevice.h',['../EdgeDevice_8h.html',1,'']]],
-  ['edgestatusdto_2ehpp_4',['EdgeStatusDto.hpp',['../EdgeStatusDto_8hpp.html',1,'']]],
-  ['error_2ecpp_5',['Error.cpp',['../Error_8cpp.html',1,'']]],
-  ['error_2eh_6',['Error.h',['../Error_8h.html',1,'']]],
-  ['example_2ecpp_7',['example.cpp',['../example_8cpp.html',1,'']]],
-  ['example_2eh_8',['example.h',['../example_8h.html',1,'']]]
+  ['edgescontroller_2ecpp_4',['EdgesController.cpp',['../EdgesController_8cpp.html',1,'']]],
+  ['edgescontroller_2ehpp_5',['EdgesController.hpp',['../EdgesController_8hpp.html',1,'']]],
+  ['edgestatusdto_2ehpp_6',['EdgeStatusDto.hpp',['../EdgeStatusDto_8hpp.html',1,'']]],
+  ['error_2ecpp_7',['Error.cpp',['../Error_8cpp.html',1,'']]],
+  ['error_2eh_8',['Error.h',['../Error_8h.html',1,'']]],
+  ['example_2ecpp_9',['example.cpp',['../example_8cpp.html',1,'']]],
+  ['example_2eh_10',['example.h',['../example_8h.html',1,'']]]
 ];
